a9913c6c8ad7003700cdb7444f3f7eb0fb18c4bd
Remy CASTRO <remycastro27@icloud.com>
2024-05-07 00:11:30 +0200

    show pop up map when clicking on a marker

--- src/app/components/trip/preview/widget-map/widget-map.component.ts
+++ src/app/components/trip/preview/widget-map/widget-map.component.ts
@@ -8,6 +8,8 @@ import * as L from 'leaflet'
 
 import {listTypesIcons} from "../../../../../models/trip.model";
 import 'leaflet.fullscreen/Control.FullScreen.js'
+import fr from "dayjs/locale/fr";
+import dayjs from "dayjs";
 
 
 @Component({
@@ -65,11 +67,23 @@ export class WidgetMapComponent implements OnInit {
   showActivitiesMarker() {
     this.tripService.tripSelected()?.activities?.forEach(activity => {
       if(this.map && (activity.latitude && activity.longitude)) {
-        L.marker(L.latLng(activity.latitude, activity.longitude), {
+        const marker = L.marker(L.latLng(activity.latitude, activity.longitude), {
           icon: L.divIcon({
             className: '',
             html: `<div class="w-[40px] h-[40px] bg-white rounded-full flex justify-center items-center"><img src="${this.getIconByType(activity.icon)}" class="w-[25px] h-[25px]" alt="" /></div>`})
         }).addTo(this.map);
+
+        // Pop up
+        marker.bindPopup(`
+            <div class="flex items-center gap-2">
+                <img src="${this.getIconByType(activity.icon)}" class="w-[25px] h-[25px]" alt="" />
+                  <h4 class="text-[1.15rem]">${activity.name}</h4>
+            </div>
+            <p class="first-letter:uppercase">
+              <span>${dayjs(activity.start).locale(fr).format('dddd DD MMMM à HH:mm:ss')}</span>
+            </p>
+        `)
+
       }
     })
   }
