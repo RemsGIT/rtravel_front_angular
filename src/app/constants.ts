85bb2fd02c151fe6c056f36f82a00efed924fdd9
Remy CASTRO <remycastro27@icloud.com>
2024-06-04 20:05:27 +0200

    add constant message: update participant

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -16,6 +16,7 @@ export const constants = {
     participant: {
       SUCCESS_CREATE: "Un participant a été ajouté",
       SUCCESS_DELETE: "Le participant a bien été supprimé",
+      SUCCESS_UPDATE: "Le participant a bien été modifié",
       ERROR_EXISTS: "Le participant est déjà dans le voyage"
     },
     ERROR_CREATE: "Erreur lors de la création, réessayez plus tard",
