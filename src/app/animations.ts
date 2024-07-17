dcdd40aa91530e7009844c48d64e8a6dde627160
Remy CASTRO <remycastro27@icloud.com>
2024-07-17 10:05:13 +0200

    test fix animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -6,16 +6,15 @@ import {
   query, group, animateChild, stagger, keyframes,
 } from '@angular/animations';
 
-export const fadeAnimation =
-  trigger('routeAnimations', [
-    transition(
-      '* <=> *',
-      animate(
-        '300ms cubic-bezier(0.645, 0.045, 0.355, 1)',
-        keyframes([
-          style({opacity: 0}),
-          style({opacity: 1}),
-        ]),
-      ),
-    ),
-  ]);
+export const fadeAnimation = trigger('routeAnimations', [
+  transition('* <=> *', [
+    animate(
+      '300ms cubic-bezier(0.645, 0.045, 0.355, 1)',
+      keyframes([
+        style({ opacity: 0, offset: 0 }),
+        style({ opacity: 0.5, offset: 0.5 }),
+        style({ opacity: 1, offset: 1 }),
+      ])
+    )
+  ])
+]);
